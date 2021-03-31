6bdba8d1ec5fcbec88f84d707efef987f6ddddd2
Vladislav V <0428vlad0428@mail.ru>
2021-03-31 15:50:27 +0300

    finished 2.6

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("what's up?")
+// Основы JavaScript
+alert("Я JavaScript!")
+
+// комментарий #1
+/*комментарий*/ alert( 'Мир' ); /*#2*/
+
+const bigInt = 1234567890123456789012345678901234567890424n;
+alert(typeof bigInt + bigInt)
+
+alert( `результат: ${1 + 2}` ); // результат: 3
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
+
+// Создайте страницу, которая спрашивает имя у пользователя и выводит его.
+
+personName = prompt("Как тебя зовут?");
+alert(`Тебя зовут ${personName}`)
