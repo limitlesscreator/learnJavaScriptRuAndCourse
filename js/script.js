ad66fc726cfcd3564b031c28cbdb61dab5b71c56
Vladislav V <0428vlad0428@mail.ru>
2021-09-20 15:50:01 +0300

    event loop

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
-function clear() {
-    console.clear()
-}
-
-
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1
-    if (a === 2) {
-        resolve('Success')
-    } else {
-        reject('Failed')
+function start(){
+    function clear() {
+        console.clear()
     }
-})
+
+
+    let p = new Promise((resolve, reject) => {
+        let a = 1 + 1
+        if (a === 2) {
+            resolve('Success')
+        } else {
+            reject('Failed')
+        }
+    })
 
 // p.then((message) => {
 //     console.log('This is in the then ' + message)
@@ -19,75 +20,142 @@ let p = new Promise((resolve, reject) => {
 // })
 /////////////////////////////////////////////////////////////////
 // not good example of code bellow without Promise
-{
-    const userLeft = false
-    const userWatchingCatMeme = true
+    {
+        const userLeft = false
+        const userWatchingCatMeme = true
 
-    function watchTutorialCallback(callback, errorCallback) {
-        if (userLeft) {
-            errorCallback({
-                name: 'User Left',
-                message: ':('
-            })
-        } else if (userWatchingCatMeme) {
-            errorCallback({
-                name: 'User Watching Cat Meme',
-                message: 'WebDevSimplified < Cat'
-            })
-        } else {
-            callback('Thumbs up and Subscribe')
-        }
-    }
-
-    // watchTutorialCallback((message) => {
-    //     console.log('Success: ' + message)
-    // }, (error) => {
-    //     console.log(error.name + ' ' + error.message)
-    // })
-}
-
-// good example of code bellow with Promise :)
-
-{
-    const userLeft = false
-    const userWatchingCatMeme = true
-
-    function watchTutorialPromise() {
-        return new Promise((resolve, reject) => {
+        function watchTutorialCallback(callback, errorCallback) {
             if (userLeft) {
-                resolve({
+                errorCallback({
                     name: 'User Left',
                     message: ':('
                 })
             } else if (userWatchingCatMeme) {
-                reject({
+                errorCallback({
                     name: 'User Watching Cat Meme',
                     message: 'WebDevSimplified < Cat'
                 })
             } else {
-                resolve('Thumbs up and Subscribe')
+                callback('Thumbs up and Subscribe')
             }
+        }
+
+        // watchTutorialCallback((message) => {
+        //     console.log('Success: ' + message)
+        // }, (error) => {
+        //     console.log(error.name + ' ' + error.message)
+        // })
+    }
+
+// good example of code bellow with Promise :)
+
+    {
+        const userLeft = false
+        const userWatchingCatMeme = true
+
+        function watchTutorialPromise() {
+            return new Promise((resolve, reject) => {
+                if (userLeft) {
+                    resolve({
+                        name: 'User Left',
+                        message: ':('
+                    })
+                } else if (userWatchingCatMeme) {
+                    reject({
+                        name: 'User Watching Cat Meme',
+                        message: 'WebDevSimplified < Cat'
+                    })
+                } else {
+                    resolve('Thumbs up and Subscribe')
+                }
+            })
+        }
+
+        watchTutorialPromise().then((message) => {
+            console.log('Success: ' + message)
+        }).catch((error) => {
+            // console.log(error.name + ' ' + error.message)
         })
     }
-    watchTutorialPromise().then((message) => {
-        console.log('Success: ' + message)
-    }).catch((error) => {
-        console.log(error.name + ' ' + error.message)
+
+    const recordVideoOne = new Promise((resolve, reject) => {
+        resolve('Video 1 Recorded')
     })
+
+    const recordVideoTwo = new Promise((resolve, reject) => {
+        resolve('Video 2 Recorded')
+    })
+
+    const recordVideoThree = new Promise((resolve, reject) => {
+        resolve('Video 1 Recorded')
+    })
+
+    Promise.all([recordVideoOne, recordVideoThree, recordVideoTwo]).then((messages) => {
+        // console.log(messages)
+    })
+
+//start
+//Async code example
+// setTimeout(() => {
+//     console.log('We are in the timeout')
+// },1000)
+
+    console.log('Start')
+
+    function loginUser(email, password) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({userEmail: email})
+            }, 1500)
+        })
+    }
+
+    const user = loginUser('deved@mail.ru', 123456)
+        .then(obj => {
+            console.log(obj)
+        })
+        .then(() => console.log('End'))
+        .catch(() => console.log('Error'))
+
+//Practice Call Stack
+
+    function multiply(a, b) {
+        return a * b
+    }
+
+    function square(n) {
+        return multiply(n, n)
+    }
+
+    function printSquare(n) {
+        const result = square(n)
+        console.log(result)
+    }
+
+    setInterval(function timeout() {
+        setTimeout(function timeout() {
+            console.log('3')
+        }, 1000)
+    }, 1000)
 }
 
-const recordVideoOne = new Promise((resolve,reject) => {
-    resolve('Video 1 Recorded')
-})
+console.log('start')
 
-const recordVideoTwo = new Promise((resolve,reject) => {
-    resolve('Video 2 Recorded')
-})
+setTimeout(() => {
+    console.log('timer 1')
+    setTimeout(() => {
+        console.log('timer 2')
+    },0)
+},0)
 
-const recordVideoThree = new Promise((resolve,reject) => {
-    resolve('Video 1 Recorded')
-})
+setTimeout(() => {
+    console.log('timer3')
+}, 0)
 
-Promise.all([recordVideoOne,recordVideoThree,recordVideoTwo]).then((messages) => {
-    console.log(messages)
-})
+console.log('end')
+
+//start
+//end
+//
+// callStack = [1,3,2]
+
