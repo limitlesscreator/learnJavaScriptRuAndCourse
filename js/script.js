b34605e9526ca598b3e9864f7168a78f7b7aa91c
Vladislav V <0428vlad0428@mail.ru>
2021-09-21 13:14:12 +0300

    js practice

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function start(){
+function start() {
     function clear() {
         console.clear()
     }
@@ -145,8 +145,8 @@ setTimeout(() => {
     console.log('timer 1')
     setTimeout(() => {
         console.log('timer 2')
-    },0)
-},0)
+    }, 0)
+}, 0)
 
 setTimeout(() => {
     console.log('timer3')
@@ -159,3 +159,57 @@ console.log('end')
 //
 // callStack = [1,3,2]
 
+function CalculateString(n, nums) {
+    let result = []
+    for (let i = 0; i < Math.floor(n / 2); i++) {
+        let temp = nums.replace(/10/, '')
+        result.push(temp)
+        nums = temp
+    }
+    return nums.length
+}
+
+CalculateString(4, "1100")//0
+CalculateString(5, "01010")//1
+
+const removeVowels = function (str) {
+    return str.replace(/[aeiou]/g, '')
+}
+
+removeVowels("drake")//"drk"
+
+// Create a function that takes any sentence and redistributes the spaces (and adds additional spaces if needed) so that each word starts with a vowel. The letters should all be in the same order but every vowel in the sentence should be the start of a new word.
+// The first word in the new sentence may start without a vowel. It should return a string in all lowercase with no punctuation (only alphanumeric characters).
+// EXAMPLES 'It is beautiful weather today!' becomes 'it isb e a ut if ulw e ath ert od ay' 'Coding is great' becomes 'c od ing isgr e at' 'my number is 0208-533-2325' becomes 'myn umb er is02085332325'
+function vowelStart(str) {
+    let result = []
+
+    function replacer(s) {
+        return ` ${s}`
+    }
+
+    str = str.replace(/[ ]/g, '')
+    let arr = str.split('')
+    for (let i = 0; i < arr.length; i++) {
+        result.push(arr[i].replace(/[aeiou]/i, replacer))
+    }
+    return result.join('').toLowerCase().trim().replace(/[!,_-]/gi, '')
+}
+
+vowelStart('It is beautiful weather today!')//, 'it isb e a ut if ulw e ath ert od ay',)
+vowelStart('Coding is great')//'c od ing isgr e at
+vowelStart('my number is 0208-533-2325')//,'myn umb er is02085332325' ,)
+vowelStart('oranges, apples, melon, pineapple')//, 'or ang es appl esm el onp in e appl e', )
+vowelStart('under_score')//, 'und ersc or e')
+// just a little hard, but i feel so good so nice XD
+
+// \d – все цифры от 0 до 9.
+// \D – не цифры.
+// \s – пробельные символы, символ табуляции, новые строки (\n).
+// \S – все, кроме \s.
+// \w – латиница (буквы русского алфавита не подходят), цифры, подчёркивание '_'.
+// \W – все, кроме \w.
+// /./ – любой символ кроме перевода строки \n.
+//     Квантификатор – количество раз, которое встречается символ в строке подряд:
+//     + – означает «один или более» (например: /\d+/).
+// * – означает «ноль или более» (например: /\d*/).
