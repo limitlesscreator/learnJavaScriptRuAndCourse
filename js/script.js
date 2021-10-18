55e621af43268ccf71f45aef56be7e4e04b9ac00
Vladislav V <0428vlad0428@mail.ru>
2021-10-18 13:07:49 +0300

    practice obj

--- js/script.js
+++ js/script.js
@@ -109,8 +109,73 @@ function bloodAlcoholContent(drinks, weight, sex, time) {
     return +bac;
 }
 
-bloodAlcoholContent({ounces:12.5, abv:0.4}, 190, 'male', 1)// 0.0837)
-bloodAlcoholContent({ounces:180, abv:0.05}, 160,'female', 1)// 0.1758);
-bloodAlcoholContent({ounces:50, abv:0.14}, 250,'male', 3)// 0.0601);
-bloodAlcoholContent({ounces:20, abv:0.4}, 100,'female', 2)// 0.2414);
+bloodAlcoholContent({ounces: 12.5, abv: 0.4}, 190, 'male', 1)// 0.0837)
+bloodAlcoholContent({ounces: 180, abv: 0.05}, 160, 'female', 1)// 0.1758);
+bloodAlcoholContent({ounces: 50, abv: 0.14}, 250, 'male', 3)// 0.0601);
+bloodAlcoholContent({ounces: 20, abv: 0.4}, 100, 'female', 2)// 0.2414);
+clear()
+
+function giveMeFive(obj) {
+    let result = []
+    for (let key in obj) {
+        if (obj[key].length === 5 && key.length !== 5) {
+            result.push(obj[key])
+            console.log(1)
+        } else if (key.length === 5 && obj[key].length !== 5) {
+            console.log(key.length + ' ' + obj[key].length)
+            result.push(key)
+            console.log(2)
+        } else if (key.length === 5 && obj[key].length === 5) {
+            result.push(key)
+            result.push(obj[key])
+            console.log(3)
+        }
+    }
+    return result
+}
+
+giveMeFive({Our: "earth", is: "a", beautyful: "world"})//["earth","world"]);
+giveMeFive({Ihave: "enough", money: "to", buy: "a", car: "model"})//["Ihave","money","model"])
+giveMeFive({Pears: "than", apple: "sweet"})//["Pears","apple","sweet"])
+
+// Think of a way to store the languages as a database (eg an object). The languages are listed below so you can copy and paste!
+function greet(language) {
+    let languageArr = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+    return languageArr[language] || 'Welcome'
+}
+
+greet('english')//, 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+greet('dutch')//, 'Welkom', "Your function should have returned 'Welkom'. Try again.");
+greet('IP_ADDRESS_INVALID')//, 'Welcome', "Your function should have returned 'Welcome'. Try again.");
+
+function duckDuckGoose(players, goose) {
+    for (let i = 0; i < 100; i++) {
+        if (goose > players.length) {
+            goose -= players.length
+        } else break
+    }
+    return result = players[goose - 1]
+}
+
+duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 30)//z
+duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 28)//, "g");
+duckDuckGoose(["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"], 12)//, "b");
 
