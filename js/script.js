597f661e9d82fef7c500d355f7fc54f5f2255045
Vladislav V <0428vlad0428@mail.ru>
2022-01-05 15:34:59 +0300

    OOP and constructor functions

--- js/script.js
+++ js/script.js
@@ -43,10 +43,80 @@ const Person = function(firstName, birthYear){
     this.birthYear = birthYear
 }
 
-const jonas = new Person('Jonas', 1991)
-console.log(jonas)
+// !! never create methods inside constructor functions, if you do, it'll be bad for code performance
 
 // 1. New {} is created
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
+const jonas = new Person('Jonas', 1991)
+console.log(jonas)
+
+const matilda = new Person('Matilda',2017)
+const jack = new Person('Jack',1975)
+console.log(jonas instanceof Person)
+console.log(matilda)
+
+// Prototypes
+console.log(Person.prototype)
+
+Person.prototype.calcAge = function(){
+    console.log(2037 - this.birthYear)
+}
+
+jonas.calcAge()
+
+console.log(jonas.__proto__)
+console.log(jonas.__proto__ === Person.prototype)
+console.log(Person.prototype.isPrototypeOf(jonas))
+
+console.log(jonas)
+// setTimeout(() => {
+    Person.prototype.species = 'Homo Sapiens'
+// },5000)
+
+console.log(jonas.hasOwnProperty('species'))
+console.log(jonas.hasOwnProperty('firstName'))
+console.log(jonas.__proto__.__proto__ === Object.prototype)
+
+console.dir(Person.prototype.constructor)
+
+const arr = [3, 6, 4, 5, 6, 9, 3, 3]
+console.log(arr.__proto__ === Array.prototype)
+
+Array.prototype.unique = function(){
+    return [...new Set(this)]
+}
+
+console.log(arr.unique())
+console.dir(x => x + 1)
+
+function Car(mark,speed){
+    this.mark = mark
+    this.speed = speed
+}
+Car.prototype.accelerate = function(){
+    this.speed += 10
+    console.log(this.speed)
+}
+
+Car.prototype.brake = function(){
+    this.speed -= 5
+    console.log(this.speed)
+}
+
+const car1 = new Car('BMW',120)
+const car2 = new Car('Mercedes',95)
+
+car1.accelerate()
+car1.accelerate()
+car2.brake()
+console.log(car1)
+console.log(car2)
+ // function changeColor(){
+//     this.style.color = 'white'
+// }
+// const button = document.querySelector('.buy')
+// // const button2 = document.querySelector('.poll')
+// const click1 = button.ad dEventListener('click',() => changeColor.call(button))
+
