cafac89455a598f7e4e194eab263f3ea7fae56e0
Vladislav V <0428vlad0428@mail.ru>
2022-02-05 20:37:06 +0300

    Repeat closures, strict mode, arguments, spread/rest, NaN,

--- js/script.js
+++ js/script.js
@@ -37,8 +37,8 @@ const YELLOW = Symbol('yellow')
 const BLUE = Symbol('blue')
 const cat = 'blue'
 
-function getThreatLevel(color){
-    switch (color){
+function getThreatLevel(color) {
+    switch (color) {
         case RED:
             return 'severe'
         case ORANGE:
@@ -50,34 +50,34 @@ function getThreatLevel(color){
         default:
             console.log(`I don't know that color!`)
     }
- }
+}
 
- console.log(getThreatLevel(cat))
+console.log(getThreatLevel(cat))
 
 console.clear()
 
 const length = Symbol('length')
 
-class Train{
+class Train {
     constructor() {
         this[length] = 0
     }
 
-    add(car, contents){
+    add(car, contents) {
         this[car] = contents
         this[length]++
     }
 
-    showLength(){
+    showLength() {
         console.log(this[length])
     }
 }
 
 let freightTrain = new Train()
-freightTrain.add('refrigerator car','cattle')
-freightTrain.add('flat car','aircraft parts')
+freightTrain.add('refrigerator car', 'cattle')
+freightTrain.add('flat car', 'aircraft parts')
 freightTrain.add('tank car', 'milk')
-for (let car in freightTrain){
+for (let car in freightTrain) {
     console.log(car, freightTrain[car])
 }
 console.log(Object.getOwnPropertySymbols(freightTrain))
@@ -91,7 +91,7 @@ let cat2 = {
     length: 0.5,
     color: 'white',
     age: 7,
-    getVoice(){
+    getVoice() {
         console.log('maaau')
     }
 }
@@ -107,24 +107,27 @@ b.some = '123'
 
 console.log(b)
 
-console.log(['a','b'])
-console.log({0:'a',1:'b',length: 2})
+console.log(['a', 'b'])
+console.log({0: 'a', 1: 'b', length: 2})
 
 console.log(document.querySelector('.images'))
 
 console.dir(document.querySelector('.images'))
 console.clear()
-class ModernCat{
-    constructor(length,color,weight){
+
+class ModernCat {
+    constructor(length, color, weight) {
         this.weight = weight
         this.color = color
         this.length = length
     }
-    eat(){
+
+    eat() {
         console.log(`has started eat`)
     }
 }
-let modernCat1 = new ModernCat(0.5,'black','5kg')
+
+let modernCat1 = new ModernCat(0.5, 'black', '5kg')
 
 console.log(modernCat1)
 
@@ -147,31 +150,90 @@ console.log(modernCat1)
 // new syntax
 {
     class Animal {
-        constructor(age){
+        constructor(age) {
             this.year = age
         }
     }
 
-    class Cat extends Animal  {
-        constructor(age){
+    class Cat extends Animal {
+        constructor(age) {
             super(age) // Вызов родительского конструктора
             this.color = 'black'
         }
     }
+
     const cat = new Cat(5)
     console.log(cat)
 
-    class Tiger extends Cat{
-        constructor(age){
+    class Tiger extends Cat {
+        constructor(age) {
             super(age)
             this.stripers = 'long'
         }
     }
+
     let tiger = new Tiger(2)
     console.log(tiger)
-    console.log(['a','b','c'].indexOf('b'))
+    console.log(['a', 'b', 'c'].indexOf('b'))
     console.dir(Array)
 
-    let getElements = document.querySelectorAll('.button')
+    let getElements = document.querySelectorAll('.button') //.indexOf('button.button') error because
+    // above is returning an Array with NodeList
     console.log(getElements)
 }
+
+console.clear()
+
+// new Function
+let func = new Function('console.log(`test`)')
+console.log(func(1, 2))
+
+//closures
+let name = "John";
+
+function sayHi() {
+    alert("Hi, " + name);
+}
+
+name = "Pete";
+sayHi()
+
+function makeWorker() {
+    let name = "Pete";
+
+    return function () {
+        alert(name);
+    };
+}
+
+let name = "John";
+
+// create a function
+let work = makeWorker();
+// if we call work, it's going be an error
+
+// Not strict mode
+function newCode() {
+    'use strict'
+    // Strict mode
+}
+
+{
+    var let = 1 // no error without strict mode
+    console.log(let)
+}
+
+console.clear()
+
+function sum(a, b, ...arg) {
+    // console.log(arguments.slice(1))  Error because prototype is Object class:D
+    console.log(Array.prototype.slice.call(arguments, 0))
+    console.log(arguments)
+    return a + b
+}
+
+sum(1, 2, 3, 4, 5)
+
+
+console.log(typeof NaN) // number lmao
+console.log(NaN === NaN) // false
