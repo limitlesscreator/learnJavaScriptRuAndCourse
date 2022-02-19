4ad2aa2c3e464327f9c262a1a32afdcca83a259f
Vladislav V <0428vlad0428@mail.ru>
2022-02-19 20:39:55 +0300

    operators / hoisting

--- js/script.js
+++ js/script.js
@@ -119,9 +119,97 @@ console.log(user.name)
 let a = {
     name: 'Vova'
 }
-
+//
 let b = {
     name: 'Vova'
 }
 
 console.log(JSON.stringify(a) === JSON.stringify(b)) // going be true
+
+console.clear()
+
+//new day, new knowledge :D
+// continue the book: The JavaScript interview Workbook: 400 Coding exercises
+
+// Operator and Comparisons
+
+// 7 types of operators: typeof, string, void, assigment, comparisons, logical, arithmetic
+
+// Operator of the same precedence or the same priority level, operations are performed left to right
+
+{
+    // Arithmetic operators
+    let cookies = 10
+    console.log(cookies++) // still 10
+    console.log(cookies) // 11
+    //but here
+    {
+        let cookies = 100;
+        console.log(--cookies) // 99
+        console.log(cookies) // 99
+    }
+    for(let i = 0; i <= 10; i++){
+        console.log(++i) // 1 3 5 7 9 11
+    }
+
+    // Assignment operators - right to left
+    let num1 = 100
+    let num2 = 30
+    let num3 = 80
+    num1 = num2 = num3
+    console.log(num3, num2, num1) // 80 80 80
+
+    let x = 10;
+    let y = 20;
+    x+= y*= 3;
+    console.log(x); // 70
+
+    // Void operator - return undefined
+
+    // Hoisting
+
+    // Adding variable and function declarations to memory during compilation before they get executed is called 'hoisting'
+    // during declaration (var x;), variables are automatically assigned of a value undefined this is called initialization
+    // assignment refers to assigning a value to a variable
+
+    {
+        console.log(icecream); // undefined
+        var icecream = 'Maple Syrup'
+    }
+    // the variable var icecream is hoisted to the top of its scope
+
+    for(var i = 0; i <= 4; i++) {
+        console.log(i);
+        setTimeout(function() {
+            console.log('The number being logged is ' + i);
+        },1000);
+    }
+    // 5 5 5 5 5 5
+
+    // let and const hoisting
+    // var is stored undefined, but let and const stored (uninitialized -damn it's hard word to memorized how to write :D)
+    // values. var let const are hoisted but without values in memory even undefined.
+
+    // console.log(giraffe)
+    const giraffe = 'Masai giraffe'
+    {
+        const giraffe = 'Nubia giraffe'
+    }
+    console.log(giraffe)
+
+    // Function hoisting
+    // Functions are hoisted along wit a reference to the body of the entire function.
+    // that's why code bellow is work
+
+    myFunc()
+    function myFunc(){
+        console.log('This is myFunc')
+    }
+    /////////////////////////////////////////////////
+    // Precedence for variable and function hoisting|
+    // 1. Variable assignment                       |
+    // 2. Function declaration                      |
+    // 3. Variable declaration                      |
+    ////////////////////////////////////////////////|
+}
+
