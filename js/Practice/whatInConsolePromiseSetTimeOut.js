cef305d3c1f37454caf7f18fb899c945c9b53317
Vladislav V <0428vlad0428@mail.ru>
2022-07-10 19:36:51 +0300

    preparation

--- js/Practice/whatInConsolePromiseSetTimeOut.js
+++ js/Practice/whatInConsolePromiseSetTimeOut.js
@@ -14,6 +14,9 @@
 // });
 //
 // console.log("5");
+{
+    answer: '1 5 2 name 4 3'
+}
 
 //////////////////////////////////////////////////////////////
 
@@ -27,13 +30,17 @@
 //     console.log(4);
 //     resolve();
 // }).then(() => console.log(5));
-
-
+//
+{
+    answer: '3 4 2 5 1'
+}
 //////////////////////////////////////////////////////////////
 
 // setTimeout(() => console.log(1), 0)
 //
-// const p = Promise.resolve().then(() => console.log(2)).finally(() => console.log(3));
+// const p = Promise.resolve()
+//     .then(() => console.log(2))
+//     .finally(() => console.log(3));
 //
 // console.log(4);
 //
@@ -43,3 +50,47 @@
 //     console.log(6);
 //     resolve();
 // }).then(() => console.log(7));
+
+{
+    answer: '4 6 2 7 3 5 1'
+}
+
+//////////////////////////////////////////////////////////////
+
+// const one = () => Promise.resolve('One!')
+//
+// async function myFunc(){
+//     console.log('In function!')
+//     const res = await one()
+//     console.log(res)
+// }
+//
+// console.log('Before function')
+// myFunc()
+// console.log('After function!')
+
+{
+    answer: 'Before function! In function! AfterFunction! One! '
+}
+
+//////////////////////////////////////////////////////////////
+
+// console.log(1);
+//
+// setTimeout(() => console.log(2));
+//
+// Promise.resolve().then(() => console.log(3)).then(() => console.log(33)).then(() => console.log(333));
+//
+// Promise.resolve().then(() => setTimeout(() => console.log(4)));
+//
+// Promise.resolve().then(() => console.log(5)).then(() => console.log(10)).then(() => console.log(11));
+//
+// setTimeout(() => console.log(6));
+//
+// console.log(7);
+
+{
+    answer: '1 7 3 5 33 10 333 11 2 6 4'
+}
+
+//macro que: 6 4
--- script.js
+++ script.js
@@ -1,1 +1,76 @@
+let elem = document.getElementById('div2')
+let style = window.getComputedStyle(elem).color
 
+elem.setAttribute('value', '28')
+console.log(elem.getAttribute('value'))
+elem.removeAttribute('style')
+console.log(elem)
+
+let test2 = document.createElement('div')
+test2.innerHTML = `Test2`
+test2.setAttribute('id','test2')
+
+let childTest1 = document.createElement('span')
+let childTest2 = document.createElement('span')
+childTest1.innerHTML = 'Child 1'
+childTest2.innerHTML = 'Child 2'
+
+test2.append(childTest1)
+test2.prepend(childTest2)
+console.log(test2)
+
+
+elem.after(test2)
+
+
+// clear interval repeating
+
+let startInterval = document.getElementById('startI')
+let stopInterval = document.getElementById('cancelI')
+let count = 0
+let firstInterval
+
+function intervalFunction() {
+    console.log(count)
+    count++
+}
+
+function start(){
+   firstInterval = setInterval(intervalFunction,1000)
+}
+
+function stop(){
+    clearInterval(firstInterval)
+}
+
+
+// const firstInterval =  setInterval(intervalFunction,1000)
+startInterval.addEventListener('click', start)
+stopInterval.addEventListener('click', stop)
+
+// finished reading on page 34 you don't know js Async
+
+console.log(window.getComputedStyle(elem).fontSize)
+console.clear()
+let store1 = new Set([1,2,3,4])
+let store2 = {
+    'one': 1,
+    'two': '2',
+    [Symbol.iterator](n = 10){
+        let i = 0
+        return {
+            next(){
+                if(i < n){
+                    return {value: ++i, done: false}
+                }
+                return {value: undefined, done: true}
+            }
+        }
+    }
+}
+
+// for (let elem of store2){
+//     console.log(elem)
+// }
+
+console.log(store2.next())
